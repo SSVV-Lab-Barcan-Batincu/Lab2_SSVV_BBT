281cbcb4c78cc2f976435c9eaf5f61a940dbe7e5
Barcoaie <florinel.barcan@gmail.com>
2021-03-30 11:50:50 +0300

    added tests

--- src/test/java/ssvv/barcanbatincu/AppTest.java
+++ src/test/java/ssvv/barcanbatincu/AppTest.java
@@ -3,6 +3,8 @@ package ssvv.barcanbatincu;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import ssvv.barcanbatincu.domain.Student;
+import ssvv.barcanbatincu.repository.StudentRepo;
 
 /**
  * Unit test for simple App.
@@ -16,5 +18,15 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void addStudentTest1() {
+        StudentRepo studentRepo = new StudentRepo();
+        studentRepo.addStudent(new Student(1234,"abc", "333"));
+        assert !studentRepo.getStudentList().isEmpty();
+        assert studentRepo.getStudentList().get(0).getId() == 1234;
+        assert studentRepo.getStudentList().get(0).getName().equals("abc");
+        assert !studentRepo.getStudentList().get(0).getName().equals("333");
     }
 }
